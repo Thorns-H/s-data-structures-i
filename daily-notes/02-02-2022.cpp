a65d23d611c240e9b031d5bbd1867b614e3acbef
Thorns-H <abrahamm9986@gmail.com>
2022-02-05 13:30:08 -0600

    Check last Lista.h header

---
+++ daily-notes/02-02-2022.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+/*
+    - Seminario de Estructura de Datos I
+    - Abraham Magaña Hernández - 220791217
+    - Clase del 04-02-2022 
+*/
+
+#ifdef _WIN32 // Declaring constants for Windows
+
+#define clear "cls"
+#define pause "PAUSE"
+
+#else // Declaring constants for Linux
+
+#define clear "clear"
+#define pause "read -p '' var"
+
+#endif
+
+using std::string;
+
+// $ g++ 04-02-2022.cpp && ./a.out && rm a.out
+
+int main(){
+
+    // Notes from this class are about the last (28-01-2022).
+
+    return 0;
+}
